2f1737594eadcd84f264913374fc717fc7c7413a
ImmerAlex <aleximm68@gmail.com>
2024-05-21 19:29:26 +0200

    add the logic of binary tree

--- src/model/binary_tree/Node.java
+++ src/model/binary_tree/Node.java
@@ -1,5 +1,41 @@
 package model.binary_tree;
 
 public class Node {
+    private int value;
+    private Node left;
+    private Node right;
 
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void addRecursive(int value) {
+        if (value < this.value) {
+            if (left == null) {
+                left = new Node(value);
+            } else {
+                left.addRecursive(value);
+            }
+        } else {
+            if (right == null) {
+                right = new Node(value);
+            } else {
+                right.addRecursive(value);
+            }
+        }
+    }
+
+    public int getMax() {
+        if (right == null) {
+            return value;
+        } else {
+            return right.getMax();
+        }
+    }
 }
--- src/model/binary_tree/Tree.java
+++ src/model/binary_tree/Tree.java
@@ -1,5 +1,17 @@
 package model.binary_tree;
 
 public class Tree {
+    private Node root;
 
+    public Tree() {
+        root = null;
+    }
+
+    public void add(int value) {
+        if (root == null) {
+            root = new Node(value);
+        } else {
+            root.addRecursive(value);
+        }
+    }
 }
