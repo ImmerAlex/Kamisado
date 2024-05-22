e35fe358367135231f8e2f765beda4c7bd545da3
Floryan Bornet <93878409+BornetFloryan@users.noreply.github.com>
2024-05-22 11:13:32 +0200

    Add files via upload

--- src/model/HoleStageFactory.java
+++ src/model/HoleStageFactory.java
@@ -1,5 +1,6 @@
 package model;
 
+import boardifier.model.ContainerElement;
 import boardifier.model.GameStageModel;
 import boardifier.model.StageElementsFactory;
 import boardifier.model.TextElement;
@@ -10,20 +11,21 @@ import java.util.List;
 /**
  * HoleStageFactory must create the game elements that are defined in HoleStageModel
  * WARNING: it just creates the game element and NOT their look, which is done in HoleStageView.
- * <p>
+ *
  * If there must be a precise position in the display for the look of a game element, then this element must be created
  * with that position in the virtual space and MUST NOT be placed in a container element. Indeed, for such
  * elements, the position in their virtual space will match the position on the display. For example, in the following,
  * the black pot is placed in 18,0. When displayed on screen, the top-left character of the black pot will be effectively
  * placed at column 18 and row 0.
- * <p>
+ *
  * Otherwise, game elements must be put in a container and it will be the look of the container that will manage
  * the position of element looks on the display. For example, pawns are put in a ContainerElement. Thus, their virtual space is
  * in fact the virtual space of the container and their location in that space in managed by boardifier, depending of the
  * look of the container.
+ *
  */
 public class HoleStageFactory extends StageElementsFactory {
-    private final HoleStageModel stageModel;
+    private HoleStageModel stageModel;
 
     public HoleStageFactory(GameStageModel gameStageModel) {
         super(gameStageModel);
@@ -33,7 +35,7 @@ public class HoleStageFactory extends StageElementsFactory {
     @Override
     public void setup() {
         TextElement text = new TextElement(stageModel.getCurrentPlayerName(), stageModel);
-        text.setLocation(0, 0);
+        text.setLocation(0,0);
         stageModel.setPlayerName(text);
 
         HoleBoard board = new HoleBoard(0, 1, 8, 8, stageModel);
@@ -42,7 +44,7 @@ public class HoleStageFactory extends StageElementsFactory {
 
         List<Pawn> OPawns = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
-            Pawn pawn = new Pawn(0, 8 - i, 'O', stageModel);
+            Pawn pawn = new Pawn(0, 8-i, 'O', stageModel);
             OPawns.add(pawn);
             board.addElement(pawn, 0, i);
         }
@@ -50,7 +52,7 @@ public class HoleStageFactory extends StageElementsFactory {
 
         List<Pawn> XPawns = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
-            Pawn pawn = new Pawn(0, i + 1, 'X', stageModel);
+            Pawn pawn = new Pawn(0, i+1, 'X', stageModel);
             XPawns.add(pawn);
             board.addElement(pawn, 7, i);
         }
